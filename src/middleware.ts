5a6750f98bac9303edb549400d0afdadd275b085
Bobby Aleem <bobbybeatmaker@hotmail.com>
2025-03-21 21:38:36 +0500

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -67,5 +67,5 @@ export default withAuth(
 );
 
 export const config = {
-    matcher: ['/', '/login', '/dashboard/:path*', '/api/:path*']
-}
+  matcher: ["/", "/login", "/dashboard/:path*", "/api/:path*"],
+};
